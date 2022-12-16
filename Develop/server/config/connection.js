1a707e4ecf90d886c7de3a522e0285e3069a4e2f
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-12-15 19:04:13 -0800

    deployment

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -8,3 +8,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
 });
 
 module.exports = mongoose.connection;
+
+mongodb+srv://dthiel22:Password111@cluster0.tchwrff.mongodb.net/?retryWrites=true&w=majority
