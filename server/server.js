a6a3f7876acb628bef8641b9c744e81d59faffa9
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-12-23 13:11:09 -0800

    updates

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const cors = require('cors')
 
 const { authMiddleware } = require('./utils/auth');
 
@@ -15,15 +16,21 @@ const server = new ApolloServer({
   // context: authMiddleware,
 });
 
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'build')));
+// }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
